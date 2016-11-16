86791d9cf5954829edcc4e8170342573588d13b9
PabloZed <pablo.gonzalez@bitmonlab.com>
2016-11-16 22:00:26 +0100

    close #3 order notes by title,update,create

--- EvernoteApp/app/src/main/java/com/example/pjgg/evernoteapp/converter/Converter.java
+++ EvernoteApp/app/src/main/java/com/example/pjgg/evernoteapp/converter/Converter.java
@@ -1,11 +1,12 @@
 package com.example.pjgg.evernoteapp.converter;
 
 import java.util.Collection;
+import java.util.List;
 
 public interface Converter<T,K> {
 
     T createDataTransferObject(K entity);
 
-    Collection<T> createDataTransferObjects(Collection<K> applications);
+    List<T> createDataTransferObjects(Collection<K> applications);
 
 }
--- EvernoteApp/app/src/main/java/com/example/pjgg/evernoteapp/model/AppNote.java
+++ EvernoteApp/app/src/main/java/com/example/pjgg/evernoteapp/model/AppNote.java
@@ -1,6 +1,8 @@
 package com.example.pjgg.evernoteapp.model;
 
-public class AppNote {
+import java.util.Comparator;
+
+public class AppNote implements Comparable<AppNote>{
 
     private String guid;
     private String title;
@@ -92,4 +94,68 @@ public class AppNote {
     public void setNotebookGuid(String notebookGuid) {
         this.notebookGuid = notebookGuid;
     }
+
+    public int compareTo(AppNote appNote) {
+
+        String guid = ((AppNote) appNote).getGuid();
+
+        //ascending order
+        return (this.guid.equalsIgnoreCase(guid))?0:1;
+
+    }
+
+    public static Comparator<Object> titleComparator = new Comparator<Object>() {
+
+        public int compare(Object appNote1, Object appNote2) {
+
+            String title1 = ((AppNote)appNote1).getTitle().toUpperCase();
+            String title2 = ((AppNote)appNote2).getTitle().toUpperCase();
+
+            //ascending order
+            return title1.compareTo(title2);
+
+            //descending order
+            //return title2.compareTo(title1);
+        }
+
+    };
+
+    public static Comparator<Object> updateNoteComparator = new Comparator<Object>() {
+
+        public int compare(Object one, Object another) {
+
+            int returnVal = 0;
+            AppNote t = (AppNote) one;
+            AppNote t1 = (AppNote) another;
+
+            if(t.getUpdated() < t1.getUpdated()){
+                returnVal =  -1;
+            }else if(t.getUpdated() > t1.getUpdated()){
+                returnVal =  1;
+            }else if(t.getUpdated() == t1.getUpdated()){
+                returnVal =  0;
+            }
+            return returnVal;
+        }
+
+    };
+
+    public static Comparator<Object> createDateComparator = new Comparator<Object>() {
+
+        public int compare(Object one, Object another) {
+            int returnVal = 0;
+            AppNote t = (AppNote) one;
+            AppNote t1 = (AppNote) another;
+
+            if(t.getCreated() < t1.getCreated()){
+                returnVal =  -1;
+            }else if(t.getCreated() > t1.getCreated()){
+                returnVal =  1;
+            }else if(t.getCreated() == t1.getCreated()){
+                returnVal =  0;
+            }
+            return returnVal;
+        }
+
+    };
 }
